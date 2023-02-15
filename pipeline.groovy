198a9435a606d9992482c1d631356042f4805fa7
Neha-oss702 <nbachchhere@gmail.com>
2023-02-15 13:08:18 +0530

    just add

--- pipeline.groovy
+++ pipeline.groovy
@@ -4,12 +4,8 @@ pipeline {
     stages {
         stage('Hello') {
             steps {
-<<<<<<< HEAD
-                echo 'well come to new hhbujof word hiuohiuhihuoihhihihibhiou  World'
-=======
-                echo 'well come to new word  World'
->>>>>>> 66b5829722b14ec455bd1adcaa120d05610ac3d0
-            }
+                echo 'well come to new hhbujof word hiuohiuhihuoihhihihibhio'
+            }             
         }
     }
 }
