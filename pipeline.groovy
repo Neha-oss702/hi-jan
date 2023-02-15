8ff0355c5d23781362232d6dc0d510493056de73
Neha-oss702 <nbachchhere@gmail.com>
2023-02-15 13:13:57 +0530

    ghduhbjnkj

--- pipeline.groovy
+++ pipeline.groovy
@@ -7,5 +7,20 @@ pipeline {
                 echo 'well come to new hhbujof word hiuohiuhihuoihhihihibhio'
             }             
         }
+        stage('build') {
+            steps {
+                echo 'well build  hiuohiuhihuoihhihihibhio'
+            }             
+        }
+        stage('test') {
+            steps {
+                echo 'well test d hiuohiuhihuoihhihihibhio'
+            }             
+        }
+        stage('deploy') {
+            steps {
+                echo 'thios kldee'
+            }             
+        }
     }
 }
